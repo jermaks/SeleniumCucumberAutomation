4c3b5f7c0910c614af5529ba7fbd48892f26349f
vp <vp1836@gmail.com>
2024-04-29 14:22:27 +0300

    rename test

--- src/test/java/com/test/TestFirst.java
+++ src/test/java/com/test/TestFirst.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class FirstTest {
+public class TestFirst {
 
     @Test
     public void testOpenBrowser()  {
