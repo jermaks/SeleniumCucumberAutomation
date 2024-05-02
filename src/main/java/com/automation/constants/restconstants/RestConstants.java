2afeb77266aabf2e8d16c2d286049344dd7be2af
vp <vp1836@gmail.com>
2024-05-02 17:15:31 +0300

    add changes to rest test

---
+++ src/main/java/com/automation/constants/restconstants/RestConstants.java
@@ -0,0 +1,7 @@
+package com.automation.constants.restconstants;
+
+public class RestConstants {
+
+    public static final String SINGLE_USER_URI = "https://reqres.in/api/users";
+
+}
--- src/main/java/com/automation/rest/RestHelper.java
+++ src/main/java/com/automation/rest/RestHelper.java
@@ -5,26 +5,17 @@ import io.cucumber.core.internal.com.fasterxml.jackson.core.JsonProcessingExcept
 import io.cucumber.core.internal.com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Invocation;
-import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+
+import static com.automation.constants.restconstants.RestConstants.SINGLE_USER_URI;
 
 public class RestHelper {
-
-    private static final String REST_URI = "https://reqres.in/api/users";
-
     public static UserModel getJsonToUser(String id) {
         Client restClient = ClientBuilder.newClient();
         return restClient
-                .target(REST_URI)
+                .target(SINGLE_USER_URI)
                 .path(String.valueOf(id))
                 .request(MediaType.APPLICATION_JSON)
                 .get(UserModel.class);
-    }
-
-    public static UserModel jsonToUser(String json) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(json, UserModel.class);
     }
 }
