df7c6fff858b7d15e7d59cecbcb54ac51b887de9
vp <vp1836@gmail.com>
2024-05-08 13:52:24 +0300

    add slf4j logger

---
+++ src/main/java/com/automation/utils/helpers/logger/LoggerHelper.java
@@ -0,0 +1,8 @@
+package com.automation.utils.helpers.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerHelper {
+    public static Logger LOGGER = LoggerFactory.getLogger(LoggerHelper.class);
+}
--- src/test/java/com/automation/modules/practice/TestFirstRest.java
+++ src/test/java/com/automation/modules/practice/TestFirstRest.java
@@ -7,13 +7,17 @@ import org.junit.jupiter.api.Test;
 import static com.automation.constants.restconstants.UserConstants.UserFirstNames.JANET;
 import static com.automation.constants.restconstants.UserConstants.UserLastNames.WEAVER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.automation.utils.helpers.logger.LoggerHelper.LOGGER;
 
 public class TestFirstRest implements UserContext {
 
     @Test
     public void testFirstRestTest() {
+        LOGGER.info("Preconditions: Test test");
         User user = getSingleUser("2");
+        LOGGER.info("Step 1: Check first name");
         assertEquals(JANET, user.getData().getFirst_name(), String.format("User First Name is not %s", JANET));
+        LOGGER.info("Step 1: Check last name");
         assertEquals(WEAVER, user.getData().getLast_name(), String.format("User First Name is not %s", WEAVER));
     }
 }
