91b0bfaeea0165c87b9620a750961c39d4fcd6eb
vp <vp1836@gmail.com>
2024-05-10 23:52:39 +0300

    update jenkins file

--- src/test/resources/jenkins/run-tests.groovy
+++ src/test/resources/jenkins/run-tests.groovy
@@ -6,20 +6,16 @@ import groovy.transform.Field
 def String branchName = 'master'
 
 pipeline {
-//    parameters {
-//        choice(
-//            name: 'branchName',
-//            choices: ['master', 'cucumber'])
-//    }
     parameters {
-        gitParameter branchFilter: 'origin/(.*)', defaultValue: 'master', name: 'BRANCH', type: 'PT_BRANCH'
+        choice(
+            name: 'branchName',
+            choices: ['master', 'cucumber'])
     }
     agent any
     stages {
         stage('Build') {
             steps {
-                git branch: "${params.BRANCH}", url: "https://github.com/jermaks/SeleniumCucumberAutomation.git/"
-                echo "Test Echo! ${params.BRANCH}"
+                echo "Test Echo!${params.branchName}"
                 sh 'mvn clean test'
             }
         }
