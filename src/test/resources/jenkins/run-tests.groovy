e74d931578813a6597e6dc020fe58f97404915fb
vp <vp1836@gmail.com>
2024-05-11 00:11:21 +0300

    update jenkins file

--- src/test/resources/jenkins/run-tests.groovy
+++ src/test/resources/jenkins/run-tests.groovy
@@ -6,16 +6,18 @@ import groovy.transform.Field
 def String branchName = 'master'
 
 pipeline {
-    parameters {
-        choice(
-            name: 'branchName',
-            choices: ['master', 'cucumber'])
-    }
+//    parameters {
+//        choice(
+//            name: 'branchName',
+//            choices: ['master', 'cucumber'])
+//    }
+    gitParameter branchFilter: 'origin/(.*)', defaultValue: 'master', name: 'BRANCH', type: 'PT_BRANCH'
     agent any
     stages {
         stage('Build') {
             steps {
-                echo "Test Echo!${params.branchName}"
+                git branch: "${params.BRANCH}", url: 'https://github.com/jenkinsci/git-parameter-plugin.git'
+                echo "Test Echo!${params.BRANCH}"
                 sh 'mvn clean test'
             }
         }
