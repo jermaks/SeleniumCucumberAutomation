e756740865e4b16c8a3044c2429d1a27e982c965
vp <vp1836@gmail.com>
2024-05-10 23:02:57 +0300

    update jenkins file

--- src/test/resources/jenkins/run-tests.groovy
+++ src/test/resources/jenkins/run-tests.groovy
@@ -1,6 +1,16 @@
 package jenkins
 
+import groovy.transform.Field
+
+@Field
+def String branch = 'master'
+
 pipeline {
+    parameters {
+        choice(
+            name: 'branch',
+            choices: ['master', 'cucumber', 'both'])
+    }
     agent any
     stages {
         stage('Build') {
