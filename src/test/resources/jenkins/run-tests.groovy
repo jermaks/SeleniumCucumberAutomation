ca5b0f8b2a3781b9f5f0fa1fc26cffb9225abc0d
vp <vp1836@gmail.com>
2024-05-10 23:36:31 +0300

    update jenkins file

--- src/test/resources/jenkins/run-tests.groovy
+++ src/test/resources/jenkins/run-tests.groovy
@@ -15,6 +15,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
+                git branch: "${params.branchName}", url: "https://github.com/jermaks/SeleniumCucumberAutomation.git/"
                 echo "Test Echo!"
                 sh 'mvn clean test'
             }
