c50bdba5c172e8a928f9cae5d503331b68277fea
vp <vp1836@gmail.com>
2024-05-11 17:47:07 +0300

    update jenkins file

--- src/test/resources/jenkins/run-tests.groovy
+++ src/test/resources/jenkins/run-tests.groovy
@@ -1,13 +1,34 @@
 package jenkins
 
+import groovy.transform.Field
+
+@Field
+def String branch = 'master'
+
 pipeline {
+    parameters {
+        choice(
+                name: 'branch',
+                choices: ['master', 'cucumber'])
+    }
     agent any
     stages {
+        stage('Scm checkout') {
+            steps {
+                echo "Pulling changes from branch - ${params.branch}"
+                script {
+                    git url: 'https://github.com/jermaks/SeleniumCucumberAutomation.git/', branch: "${params.branch}"
+                }
+            }
+        }
         stage('Build') {
             steps {
-                echo "Test Echo!"
-                sh 'mvn test -PREGRESSION'
-                sh 'mvn test -PSMOKE'
+                sh 'mvn clean install -DskipTests'
+            }
+        }
+        stage('Test run') {
+            steps {
+                sh 'mvn test'
             }
         }
     }
